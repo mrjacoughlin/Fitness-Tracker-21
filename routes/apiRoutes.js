faa3c8aa396f795dd0dadefe1e3d21ccd2160444
James Coughlin <mrjacoughlin@gmail.com>
2021-03-03 12:21:55 -0500

    workoutSchema created, put & get requests updated and tested

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -41,3 +41,35 @@ router.get("/api/workouts", (req, res) => {
       res.json(err);
     });
 });
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
